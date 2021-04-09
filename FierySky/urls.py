278cebeb1b7842cc83350a711581a84e9f067dbf
chipseater <74137062+chipseater@users.noreply.github.com>
2021-04-09 09:58:24 +0200

    main core of the project

---
+++ FierySky/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('store/', include('store.urls')),
+    path('', include('home.urls')),
+]
+
